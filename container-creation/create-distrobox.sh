9eb74575fada34f5fcf49b6550737a7c9989a4ef
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2026-03-27 20:28:00 -0700

    add $USER to ollama group

--- container-creation/create-distrobox.sh
+++ container-creation/create-distrobox.sh
@@ -82,6 +82,7 @@ fi
 echo "Setting up container desktop and command line"
 pushd Scripts
   distrobox enter $DBX_CONTAINER_NAME -- sudo chown --recursive ollama:ollama $OLLAMA_MODELS_CONTAINER
+  distrobox enter $DBX_CONTAINER_NAME -- sudo usermod --append --groups ollama $USER
   distrobox enter $DBX_CONTAINER_NAME -- su $USER -c "./1_command_line_setup.sh"
 popd
 
