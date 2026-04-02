291bb949c13302496a882d756449561ce3cca742
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2026-04-02 11:54:59 -0700

    don't re-install Distrobox / Podman

--- hosting-setup/3_container_hosting.sh
+++ hosting-setup/3_container_hosting.sh
@@ -12,7 +12,15 @@ rm --force $LOGFILE
 echo "..Activating Linuxbrew"
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
-echo "..Installing podman from Linuxbrew"
+echo "..Testing for distrobox"
+if [[ "$(which distrobox | wc -l)" != "0" ]]
+then
+  echo "..distrobox is installed"
+  exit
+
+fi
+
+echo "..Installing distrobox from Linuxbrew"
 brew install distrobox podman > $LOGFILE
 
 echo "** Finished Container Hosting **"
