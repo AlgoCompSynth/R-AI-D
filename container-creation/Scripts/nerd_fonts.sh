77358746bffae121a976883739c89a4e9ca00c7b
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2026-04-05 23:54:23 -0700

    let folks know what nerd font downloader is up to!

--- container-creation/Scripts/nerd_fonts.sh
+++ container-creation/Scripts/nerd_fonts.sh
@@ -5,7 +5,10 @@ set -e
 mkdir --parents $HOME/.fonts
 pushd /tmp > /dev/null
   rm --force --recursive nerd_fonts; mkdir nerd_fonts; cd nerd_fonts
+  echo "..Downloading nerd font zipfile"
   curl -sOL https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/CascadiaCode.zip
+  echo "..Unpacking nerd font zipfile"
   unzip -qqo CascadiaCode.zip
+  echo "..Copying nerd font files to $HOME/.fonts"
   cp CaskaydiaCoveNerdFontMono-*.ttf $HOME/.fonts/
 popd > /dev/null
