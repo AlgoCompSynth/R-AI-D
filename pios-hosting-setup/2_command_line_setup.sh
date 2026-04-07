3ef564eee3584bc62845ad219d68ac28c9279b39
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2026-04-06 18:00:13 -0700

    refactored PiOS container hosting setup

--- pios-hosting-setup/2_command_line_setup.sh
+++ pios-hosting-setup/2_command_line_setup.sh
@@ -2,22 +2,33 @@
 
 set -e
 
-pushd ../container-creation/Scripts/ > /dev/null
-  ./1_command_line_setup.sh
-
-popd > /dev/null
-
+echo "** Distrobox **"
 pushd /tmp > /dev/null
   rm --force --recursive distrobox
   echo "..Cloning Distrobox source"
   git clone --quiet \
     https://github.com/89luca89/distrobox.git
   cd distrobox
-  git checkout $DISTROBOX_VERSION
 
   echo "..Installing distrobox to $HOME/.local/bin"
+  mkdir --parents $HOME/.local/bin
+
+  if [[ ! "$PATH" =~ "$HOME/.local/bin" ]]
+  then
+    export PATH=$HOME/.local/bin:$PATH
+
+  fi
+
   ./install
   echo "..which distrobox: $(which distrobox)"
   echo "..distrobox --version: $(distrobox --version)"
+
+echo "** Finished Distrobox **"
+echo ""
+
+popd > /dev/null
+
+pushd ../container-creation/Scripts/ > /dev/null
+  ./1_command_line_setup.sh
 
 popd > /dev/null
--- pios-hosting-setup/README-Setting-up-container-hosting.md
+++ pios-hosting-setup/README-Setting-up-container-hosting.md
@@ -1,15 +1,11 @@
 # Setting up Container Hosting
 
 This directory contains scripts for setting up container hosting. I currently
-test only on Raspberry Pi OS, which is based on Debian 13, and on Bluefix DX,
-which has container hosting built in. The `1_apt_packages.sh` script should
-work on any 64-bit Debian 13 or Ubuntu 24.04 LTS system. The other scripts
-should work on any 64-bit Linux system that Homebrew supports.
+test only on Raspberry Pi OS 64-bit, which is based on Debian 13 "trixie", and
+on Bluefix DX, which has container hosting built in. To set up container
+hosting on a Raspberry Pi, run the two scripts
 
-To set up container hosting, run the three scripts
-
-- `./1_apt_packages.sh`
-- `./2_command_line_setup.sh`
-- `./3_container_hosting.sh`
+1. `./1_apt_packages.sh`
+2. `./2_command_line_setup.sh`
 
 in that order.
