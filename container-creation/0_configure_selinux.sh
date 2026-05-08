bcb4669d80e0fcac975f6c769a180d87c7c85fdb
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2026-05-08 14:17:41 -0700

    add SELinux configuration script

---
+++ container-creation/0_configure_selinux.sh
@@ -0,0 +1,28 @@
+#! /usr/bin/env bash
+
+set -e
+
+echo "* Configure SELinux *"
+
+source set_container_envars
+
+# See https://podman-desktop.io/docs/podman/gpu, Linux tab, section 4
+if [[ SELINUX_ENFORCING == "true" ]]
+then
+  echo "SELinux Enfrocing has been detected."
+  echo "Executing 'sudo setsebool -P container_use_devices true' will allow container GPU access"
+  echo "Enter 'YES' to do that."
+  read -r -p "? " response
+  echo "Response was '$response'."
+
+  if [[ "$response" == "YES" ]]
+  then
+    echo "sudo setsebool -P container_use_devices true"
+    sudo setsebool -P container_use_devices true
+
+  fi
+
+fi
+
+echo "* Finished Configure SELinux *"
+echo ""
