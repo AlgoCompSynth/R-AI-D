03c2b6957c3e04e9760e49da96944b99642bb690
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2026-05-08 14:22:20 -0700

    tested on Enforcing system

--- container-creation/0_configure_selinux.sh
+++ container-creation/0_configure_selinux.sh
@@ -7,9 +7,9 @@ echo "* Configure SELinux *"
 source set_container_envars
 
 # See https://podman-desktop.io/docs/podman/gpu, Linux tab, section 4
-if [[ SELINUX_ENFORCING == "true" ]]
+if [[ $SELINUX_ENFORCING == "true" ]]
 then
-  echo "SELinux Enfrocing has been detected."
+  echo "SELinux Enforcing has been detected."
   echo "Executing 'sudo setsebool -P container_use_devices true' will allow container GPU access"
   echo "Enter 'YES' to do that."
   read -r -p "? " response
