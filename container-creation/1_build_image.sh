48ba57b5b0d05faee78847eabaa9d17d111ae68d
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2026-05-08 13:09:51 -0700

    BY DESIGN image is CPU only!!!

--- container-creation/1_build_image.sh
+++ container-creation/1_build_image.sh
@@ -10,33 +10,15 @@ source set_container_envars
 # does not appear to handle that correctly. Using CloudFlare
 # or Google nameservers works, so here they are.
 echo "Building $DBX_CONTAINER_IMAGE"
-if [[ "$COMPUTE_MODE" == "CPU" ]]
-then
-  echo "Building CPU image"
-  podman image build \
-    --dns 1.1.1.1 \
-    --dns 1.0.0.1 \
-    --dns 8.8.8.8 \
-    --file $CONTAINERFILE \
-    --tag $DBX_CONTAINER_IMAGE \
-    --squash-all \
-    .
-
-else
-
-  echo "Building NVIDIA GPU image"
-  podman image build \
-    --security-opt=label=disable \
-    --device=nvidia.com/gpu=all \
-    --dns 1.1.1.1 \
-    --dns 1.0.0.1 \
-    --dns 8.8.8.8 \
-    --file $CONTAINERFILE \
-    --tag $DBX_CONTAINER_IMAGE \
-    --squash-all \
-    .
-
-fi
+echo "Building CPU image"
+podman image build \
+  --dns 1.1.1.1 \
+  --dns 1.0.0.1 \
+  --dns 8.8.8.8 \
+  --file $CONTAINERFILE \
+  --tag $DBX_CONTAINER_IMAGE \
+  --squash-all \
+  .
 
 echo ""
 podman system prune --force
