7350e7d29a5d03709155074b529259a40d97bc7f
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2026-05-07 17:51:15 -0700

    ditch GitHub packages

--- container-creation/image-installers/github-r-packages.R
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env Rscript
-
-print("** GitHub R Packages **")
-devtools::install_github("AlgoCompSynth/eikosany", dependencies = TRUE, build_vignettes = TRUE)
-devtools::install_github("AlgoCompSynth/consonaR", dependencies = TRUE, build_vignettes = TRUE)
-devtools::install_github("Computational-Cognitive-Musicology-Lab/humdrumR", dependencies = TRUE, build_vignettes = TRUE)
-devtools::install_github("farach/huggingfaceR", dependencies = TRUE, build_vignettes = TRUE)
-print("** Finished GitHub R Packages **")
