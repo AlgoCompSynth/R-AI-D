7622525ebba1b77dda3f70030c3bbafd45e98704
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2026-05-13 22:19:35 -0700

    merge container scripts

--- container-creation/1_create_container.sh
+++ container-creation/1_create_container.sh
@@ -1,10 +1,10 @@
 #! /usr/bin/env bash
 
-echo "* Build Image *"
+echo "* Create Container *"
 
 if [[ "${#1}" < "12" ]]
 then
-  echo "The first argument must at leat 12 characters for the 'admin' password."
+  echo "The first argument must at leat 12 characters for the 'r-ai-d' password."
   exit -255
 
 fi
@@ -29,8 +29,23 @@ podman image build \
   .
 
 echo ""
-#podman system prune --force
+echo "Image built - pruning"
+podman system prune --force
 podman image list
 
-echo "* Finished Build Image *"
+./host-utilities/configure-selinux.sh
+
+podman create \
+  --cap-add=CAP_SYS_ADMIN \
+  --dns 1.1.1.1 \
+  --dns 1.0.0.1 \
+  --dns 8.8.8.8 \
+  --name $CONTAINER_NAME --replace \
+  --publish $RSTUDIO_SERVER_PORT:$RSTUDIO_SERVER_PORT \
+  $NVIDIA_FLAGS \
+  $SECURITY_FLAGS \
+  $CONTAINER_IMAGE \
+  /sbin/init
+
+echo "* Finished Create Container *"
 echo ""
