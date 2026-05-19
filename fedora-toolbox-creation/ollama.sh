eb422e17de2f747307bc5bcef6aa4fcb1331dd06
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2026-05-19 02:16:14 -0700

    fix ollama installer

--- fedora-toolbox-creation/ollama.sh
+++ fedora-toolbox-creation/ollama.sh
@@ -1,1 +1,24 @@
-../container-creation/image-installers/ollama.sh
+#! /usr/bin/env bash
+
+set -e
+
+echo "** Ollama **"
+
+# https://docs.ollama.com/linux#install
+echo "..Installing Ollama with official installer"
+curl -fsSL https://ollama.com/install.sh | sh
+
+echo "..Ollama is installed"
+echo "..Enabling ollama.service"
+systemctl enable ollama.service
+
+echo ""
+echo "..Ollama libraries"
+ls --recursive /usr/local/lib/ollama
+echo ""
+
+ollama --version
+echo ""
+
+echo "** Finished Ollama **"
+echo ""
