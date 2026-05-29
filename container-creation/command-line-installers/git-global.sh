c674baa89e7088f73756494c4115c50768f2d71b
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2026-05-29 12:32:03 -0700

    use for git commit

---
+++ container-creation/command-line-installers/git-global.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+git config --global pull.rebase false
+git config --global push.default simple
+git config --global user.email "znmeb@znmeb.net"
+git config --global user.name "M. Edward (Ed) Borasky"
+git config --global credential.helper store
+git config --global init.defaultBranch main
