4d786f49f8674be9f79b70271f3a590013c92801
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2026-06-07 16:41:31 -0700

    add XentonalAssistant dependencies

--- container-creation/command-line-installers/fedora-installs.sh
+++ container-creation/command-line-installers/fedora-installs.sh
@@ -7,9 +7,15 @@ sudo dnf --quiet --assumeyes copr enable iucar/cran
 sudo dnf --quiet --assumeyes copr enable iucar/rstudio
 sudo dnf --quiet --assumeyes install \
   R-CoprManager \
+  R-CRAN-Rdpack \
   R-CRAN-btw \
+  R-CRAN-data.table \
   R-CRAN-devtools \
+  R-CRAN-fractional \
+  R-CRAN-numbers \
+  R-CRAN-seewave \
   R-CRAN-tinytex \
+  R-CRAN-tuneR \
   R-devel \
   libcurl-devel \
   lshw \
