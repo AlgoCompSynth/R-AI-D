2feaf3f09a7a1b9d8c4dae513d4411d5dbf6ae6d
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2026-06-07 15:20:04 -0700

    don't crash if there's no systemd!!

--- container-creation/command-line-installers/ollama.sh
+++ container-creation/command-line-installers/ollama.sh
@@ -6,18 +6,8 @@ source set_container_envars
 
 # https://docs.ollama.com/linux#install
 echo "..Installing Ollama with official installer"
-curl -fsSL https://ollama.com/install.sh | sh
-
-echo "..Ollama is installed"
-echo "..Enabling ollama.service"
-sudo systemctl enable --now ollama.service
-
-ollama --version
-echo ""
-
-echo "..Pulling $BASE_MODEL and $ALT_MODEL - this will take some time"
-/usr/bin/time ollama pull $BASE_MODEL
-/usr/bin/time ollama pull $ALT_MODEL
+curl -fsSL https://ollama.com/install.sh | sh || true
+ollama --version || true
 
 echo "** Finished Ollama **"
 echo ""
