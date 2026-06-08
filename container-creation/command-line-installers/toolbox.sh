bcb00c703cf10285a549b72c2320214eefe56b0e
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2026-06-08 13:13:12 -0700

    explicit name for container on create

--- container-creation/command-line-installers/toolbox.sh
+++ container-creation/command-line-installers/toolbox.sh
@@ -3,7 +3,8 @@
 set -e
 
 toolbox rm --force fedora-toolbox
-toolbox create --assumeyes --image ghcr.io/ublue-os/fedora-toolbox
-export HERE=$PWD
-toolbox run --container fedora-toolbox "$HERE/fedora-installs.sh"
-toolbox run --container fedora-toolbox "$HERE/1_command_line_setup.sh"
+toolbox create --assumeyes \
+  --image ghcr.io/ublue-os/fedora-toolbox \
+  fedora-toolbox
+toolbox run --container fedora-toolbox "./fedora-installs.sh"
+toolbox run --container fedora-toolbox "./1_command_line_setup.sh"
