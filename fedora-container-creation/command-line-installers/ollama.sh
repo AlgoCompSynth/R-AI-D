9e884059e94c70bfa68efc186e896aa74d10c400
Zorro Notorious MEB <znmeb@znmeb.net>
2026-06-18 00:20:50 +0000

    export Ollama to host

--- fedora-container-creation/command-line-installers/ollama.sh
+++ fedora-container-creation/command-line-installers/ollama.sh
@@ -11,3 +11,5 @@ echo "..Installing Ollama with official installer"
 curl -fsSL https://ollama.com/install.sh | sh
 echo "..Restarting / enabling ollama.service"
 sudo systemctl enable --now ollama.service
+echo "..Exporting ollama'
+distrobox export --bin $(which ollama)
