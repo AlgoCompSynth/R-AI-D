9de56366bce24cf24de9fa38cf4dac119a37e60d
Zorro Notorious MEB <znmeb@znmeb.net>
2026-06-24 09:42:35 -0700

    break out RStudio password setting

--- fedora-container-creation/1-re-create-fedora-distrobox.sh
+++ fedora-container-creation/1-re-create-fedora-distrobox.sh
@@ -7,10 +7,6 @@ echo "* Re-create Fedora Distrobox *"
 /usr/bin/time distrobox assemble create \
  --file fedora-distrobox.ini
 
-echo ""
-echo "You need to set an RStudio Server login password for $USER"
-distrobox enter fedora-distrobox -- sudo passwd $USER
-
 pushd command-line-installers > /dev/null
   /usr/bin/time distrobox enter fedora-distrobox  -- ./fedora-installs.sh
   /usr/bin/time distrobox enter fedora-distrobox  -- ./1-command-line-setup.sh
