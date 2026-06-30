7e2eda8dbb04b931d9643408149eb2797eab5ba2
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2026-06-29 19:27:03 -0700

    ditch container bits for now

--- ubuntu-container-creation/command-line-installers/serve-ollama.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env -S bash -l
-
-set -e
-
-mkdir --parents $HOME/Logfiles
-sudo systemctl enable --now ollama.service \
-  || OLLAMA_MODELS=$HOME/.ollama/models ollama serve > $HOME/Logfiles/ollama-serve.log 2>&1 &
