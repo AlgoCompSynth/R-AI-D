13e5b1254f740e9c079a0b4a08bf8cc4fd440d97
Zorro Notorious MEB <znmeb@znmeb.net>
2026-07-05 00:16:42 -0700

    clean up entry script

--- create-distrobox/enter-distrobox.sh
+++ create-distrobox/enter-distrobox.sh
@@ -5,7 +5,7 @@ echo "* Enter Container *"
 source set-host-envars
 distrobox enter $CONTAINER_NAME -- sudo systemctl enable --now rstudio-server.service
 distrobox enter $CONTAINER_NAME -- sudo systemctl enable --now ollama.service
-distrobox enter $CONTAINER_NAME -- su $USER -c bash --login
+distrobox enter $CONTAINER_NAME -- su $USER
 
 echo "* Finished Enter Container *"
 echo ""
