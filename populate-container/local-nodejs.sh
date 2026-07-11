1a5e55867f3ec75a163476b8ba50bf0c1695f488
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2026-07-11 14:21:38 -0700

    install machine-specific tarball

--- populate-container/local-nodejs.sh
+++ populate-container/local-nodejs.sh
@@ -3,9 +3,25 @@
 set -eu
 
 # https://nodejs.org/en/download
+export ARCH="$(uname --machine)"
+if [[ "$ARCH" == "aarch64" ]]
+then
+  export TARBALL="https://nodejs.org/dist/v26.5.0/node-v26.5.0-linux-arm64.tar.xz"
+
+elif [[ "$ARCH" == "x86_64" ]]
+then
+  export TARBALL="https://nodejs.org/dist/v26.5.0/node-v26.5.0-linux-x64.tar.xz"
+
+else
+  echo "Unsupported hardware - exit -255!"
+  exit -255
+
+fi
+
 echo "..Installing Node.js"
 curl -fsSL \
-  https://nodejs.org/dist/v26.5.0/node-v26.5.0-linux-x64.tar.xz \
+  $TARBALL \
   | tar xJf - --strip-components=1 --directory=$HOME/.local \
   > /dev/null
+echo "npm --version $(npm --version)"
 echo "..Node.js is installed locally"
