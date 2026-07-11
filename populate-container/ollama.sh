178b5655b38d45388002933290700fb1edee20c5
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2026-07-11 13:16:46 -0700

    more comments

--- populate-container/ollama.sh
+++ populate-container/ollama.sh
@@ -7,6 +7,7 @@ echo "..Installing Ollama with official installer"
 curl -fsSL https://ollama.com/install.sh | sh || true
 echo "..Forcing ollama:ollama ownership of /usr/share/ollama"
 sudo chown -R ollama:ollama /usr/share/ollama
+echo "..Enabling / starting ollama service"
 sudo systemctl enable --now ollama.service
 echo "..Ollama is installed - models:"
 echo ""
